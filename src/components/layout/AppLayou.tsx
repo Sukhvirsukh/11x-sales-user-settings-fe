48b737b6bceb0c3b1f155ec30f20a6e553f66d45
Gurwinder Singh <60137236+GurwinderMankoo@users.noreply.github.com>
2026-09-24 17:11:27 +0530

    MobileTopbar is fix now.

--- src/components/layout/AppLayou.tsx
+++ src/components/layout/AppLayou.tsx
@@ -77,7 +77,7 @@ function AppLayout() {
         <div className="flex h-screen gap-0 overflow-hidden bg-background p-5 md:pr-0">
             <Sidebar />
             <MobileTopbar />
-            <main className="-mx-5 -my-5 flex min-w-0 flex-1 flex-col overflow-y-auto p-5 md:mx-0">
+            <main className="-m-5 mt-5 md:-mt-5 flex min-w-0 flex-1 flex-col overflow-y-auto p-5 md:mx-0">
                 <Outlet />
             </main>
         </div>
